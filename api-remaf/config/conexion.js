98c1cffb607b9d3a49d1f898de0c24455ddf01bc
Alejandra179 <alejandra.maza1@gmail.com>
2022-09-30 09:03:59 -0300

    actualizacion de backend

--- api-remaf/config/conexion.js
+++ api-remaf/config/conexion.js
@@ -1,19 +1,18 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 const conexion = mysql.createConnection({
-    user: 'fz3li9q4ez3w',
-    password: 'pscale_pw_m-z58gz5MZ3AKkLFvhg0DAi1Y54juzmP1Ptv0TyJ4Gc',
-    host: 'dxr75bqu8ovs.aws-sa-east-1-1.psdb.cloud',
-    database: 'remaf',
-    ssl:{}
-    
+  user: "fz3li9q4ez3w",
+  password: "pscale_pw_m-z58gz5MZ3AKkLFvhg0DAi1Y54juzmP1Ptv0TyJ4Gc",
+  host: "dxr75bqu8ovs.aws-sa-east-1-1.psdb.cloud",
+  database: "remaf",
+  ssl: {},
 });
 
-conexion.connect((err)=>{
-    if (err) {
-        console.log('ha ocurrido un error'+ err)
-    } else {
-        console.log('la base de datos se conecto')
-    }
+conexion.connect((err) => {
+  if (err) {
+    console.log("ha ocurrido un error" + err);
+  } else {
+    console.log("la base de datos se conecto");
+  }
 });
 
 module.exports = conexion;
--- api-remaf/rutas.js
+++ api-remaf/rutas.js
@@ -1,106 +1,29 @@
-const router = require('express').Router()
-const conexion = require('./config/conexion')
+const router = require("express").Router();
 
-const cors = require('cors')
+const cors = require("cors");
+const {
+  getValues,
+  getLastValues,
+  getWithDate,
+  addValue,
+} = require("./controller");
 // asignamos las rutas
 
+// get estaciones
 
-// get estaciones 
-
-router.get('/',(req,res)=>{
-    let sql = 'select * from estaciones'
-    conexion.query(sql,(err,rows,fields)=>{
-        if (err) throw err;
-        else {
-            res.json(rows)
-        }
-    })
-})
+router.get("/", getValues);
 
 // get ultima medicion de la estacion
 
-router.get('/:id',(req,res)=>{
-    const{id} = req.params
-    let sql = `SELECT * FROM sensores,estaciones WHERE rela_estaciones=id_estaciones and id_estaciones='${id}' ORDER by id_sensores DESC LIMIT 1 `
-    conexion.query(sql,(err,rows,fields)=>{
-        if (err) throw err;
-        else {
-            res.json(rows)
-        }
-    })
-})
+router.get("/:id", getLastValues);
 
 // get todas las mediciones de un estacion en una fecha
 
-router.get('/:id/:date',(req,res)=>{
-    const{id , date} = req.params
-    let sql = `SELECT * FROM sensores,estaciones WHERE rela_estaciones=id_estaciones and id_estaciones='${id}'  and   date_estaciones like '%${date}%' ORDER by id_sensores DESC`
-    conexion.query(sql,(err,rows,fields)=>{
-        if (err) throw err;
-        else {
-            res.json(rows)
-        }
-    })
-})
+router.get("/:id/:date", getWithDate);
 
 //agregar medicion de sensores
 
-router.get('/:temp/:hume/:prec/:dir/:vel/:estacion' , cors() ,async function (req, res) {
-    const  { temp, hume, prec, dir, vel, estacion } = await req.params
-    if (temp == '' || !temp ) {
-        res.json({ status: 'datos incompletos' })
-    } else {
-        let sql = `insert into sensores(temperatura_sensores,
-	humedad_sensores,
-	precipitacion_sensores,
-	direcc_viento_sensores,
-	veloc_viento_sensores,
-	rela_estaciones) 
-    values('${temp}'
-    ,'${hume}'
-    ,'${prec}'
-    ,'${dir}'
-    ,'${vel}'
-    ,'${estacion}')`
-      conexion.query(sql, (err, rows, fields) => {
-        if (err) {
-            res.json(err)
-        }
-        else {
-            res.json({ status: 'Lectura de sensor agregado' })
-        }
-    })
-        }
-    })
-
 // -------------------
-router.post('/' , cors() ,async function (req, res) {
-    const  { temp, hume, prec, dir, vel, estacion } = await req.body
-    if (temp == '' || !temp ) {
-        res.json({ status: 'datos incompletos' })
-    } else {
-        let sql = `insert into sensores(temperatura_sensores,
-	humedad_sensores,
-	precipitacion_sensores,
-	direcc_viento_sensores,
-	veloc_viento_sensores,
-	rela_estaciones) 
-    values('${temp}'
-    ,'${hume}'
-    ,'${prec}'
-    ,'${dir}'
-    ,'${vel}'
-    ,'${estacion}')`
-      conexion.query(sql, (err, rows, fields) => {
-        if (err) {
-            res.json(err)
-        }
-        else {
-            res.json({ status: 'Lectura de sensor agregado' })
-        }
-    })
-        }
-    })
-
+router.post("/", cors(), addValue);
 
 module.exports = router;
