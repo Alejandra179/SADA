0629fad4282b8b22943668ce3acf401efcd1e2ad
Sebas <50669169+SebasParedes96@users.noreply.github.com>
2022-10-27 14:45:57 -0300

    Add endpoint ultima medicion por estacion

--- api-remaf/index.js
+++ api-remaf/index.js
@@ -1,47 +1,45 @@
-require("./config/conexion");
-const express = require("express");
+require('./config/conexion');
+const express = require('express');
 
-const cors = require("cors");
+const cors = require('cors')
 
-const port = process.env.PORT || 3000;
+const port = (process.env.PORT || 3000)
 
 const app = express();
 
 //admitir tipos de datos
-app.use(express.json());
+app.use(express.json())
+app.use(cors())
+
 
 // configurar cors
-app.use(cors());
 
-var allowlist = [];
+var allowlist = []
 var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (allowlist.indexOf(req.header("Origin")) !== -1) {
-    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false }; // disable CORS for this request
+    var corsOptions;
+    if (allowlist.indexOf(req.header('Origin')) !== -1) {
+      corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    } else {
+      corsOptions = { origin: false } // disable CORS for this request
+    }
+    callback(null, corsOptions) // callback expects two parameters: error and options
   }
-  callback(null, corsOptions); // callback expects two parameters: error and options
-};
 
 // configurar puerto
 
-app.set("port", port);
-
-// route heroku host:
-// https://db-remaf.herokuapp.com/api/
-// route render host:
-// wait for migration
+app.set('port',port)
 
 // rutas
 
-app.use("/api", require("./rutas"));
+app.use('/api',require('./rutas'))
+
+
 
 // iniciar express
-app.listen(app.get("port"), (error) => {
-  if (error) {
-    console.log("error al iniciar servidor: " + error);
-  } else {
-    console.log("Servidor inicado en el puerto " + port);
-  }
-});
+app.listen(app.get('port'),(error)=>{
+    if (error) {
+        console.log('error al iniciar servidor: '+ error)
+    } else {
+       console.log('Servidor inicado en el puerto '+port) 
+    }
+})
