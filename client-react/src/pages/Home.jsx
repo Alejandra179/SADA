e671d4f334bf8ccec7f5c7ff7a2bb2fbfc9894bd
Ale-Bernal <alebernalmatias@gmail.com>
2022-10-30 21:08:05 -0300

    creando tablas

--- client-react/src/pages/Home.jsx
+++ client-react/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Card from '../components/Card'
 import NavBar from '../components/Navbar'
+import Tablas from '../components/Tablas'
+
+
 export default function Home() {
   return (
     <div className='container d-flex justify-content-center align-items-center h-100'>
       <div className='row'>
       <NavBar />
       <Card />
+      <Tablas />
       </div>
     </div>
   )
