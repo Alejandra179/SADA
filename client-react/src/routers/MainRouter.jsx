286696ffec73bca97bd8d5c913e5a676834ca4df
Ale-Bernal <alebernalmatias@gmail.com>
2022-11-16 14:45:25 -0300

    pull

--- client-react/src/routers/MainRouter.jsx
+++ client-react/src/routers/MainRouter.jsx
@@ -2,13 +2,10 @@ import {
     BrowserRouter,
     Routes,
     Route
-} from 'react-router-dom';
-import { LoginScreen, RegisterScreen } from '../pages/';
-import { NotFound404 } from '../pages/NotFound404';
-import { Home } from '../pages/Home';
+} from 'react-router-dom'; 
+import { NotFound404 } from '../pages/NotFound404'; 
 import { Docs } from '../pages/Docs';
-import { EstacionesScreen } from '../pages/EstacionesScreen';
-import { OtroEnrutador } from './OtroEnrutador';
+import { EstacionesScreen } from '../pages/EstacionesScreen'; 
 import { PrivateRoutes } from './PrivateRoutes';
 import { PublicRoutes } from './PublicRoutes';
 
@@ -16,13 +13,7 @@ export const MainRouter = () => {
     return (
         <BrowserRouter>
             
-            <Routes>
-
-                <Route path='/signin' element={
-                    <PublicRoutes>
-                        <LoginScreen />
-                    </PublicRoutes>
-                } />
+            <Routes> 
                 <Route path='/estaciones' element={
                     <PublicRoutes>
                         <EstacionesScreen/>
