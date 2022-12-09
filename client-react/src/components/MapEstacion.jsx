a6e0155d761cbbd638ccaddc4c8dcc8ee9666b3a
Ale-Bernal <40031934+Ale-Bernal@users.noreply.github.com>
2022-12-09 03:50:37 -0300

    jh

--- client-react/src/components/MapEstacion.jsx
+++ client-react/src/components/MapEstacion.jsx
@@ -4,23 +4,24 @@ import imgMaps2 from '../assets/img/Estacion2.jpg'
 import Image from 'react-bootstrap/Image'
 import { useEffect } from 'react'
 import { useState } from 'react'
+import "leaflet/dist/leaflet.css"; 
 function MapEstacion(props) {
     const position = [	-26.0814025635489, -58.27586964876695]
-    const [estacionActual,setEstacionActual] = useState(props.estacionActual)
-    useEffect( () => {
-      setEstacionActual(props.estacionActual)
-    }, [props.estacionActual])
+  
     
   return (
     <>
-   <section id="facts" className="facts" data-aos="fade-up">
-          <div className="container">
-          {
-             estacionActual==1 ? <Image  src = {imgMaps1}   /> :<Image  src = {imgMaps2}   /> 
-             
-          }
-          </div>
-    </section>
+   <MapContainer center={position} zoom={300} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={position}>
+    <Popup>
+     Prueba <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
     </>
   );
 }
